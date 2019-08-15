d59dd7a6a153813c62dd2137bc1631382bbc0fd3
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-15 21:16:28 +0200

    remove ComputeSchedule APIView: this will be done on the client-side

--- schedule/views.py
+++ schedule/views.py
@@ -22,38 +22,3 @@ class DeletionsViewSet(ModelViewSet):
 class ExercisesViewSet(ModelViewSet):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
-    
-class ComputedSchedule(ListAPIView):
-    def get_queryset(self):
-        
-        # --- Get requested period ---
-        # Get settings for user
-        settings = Setting.objects.filter(user__pk=self.request.user.id)
-        # Get request params and extract ?start and &end from url
-        q = self.request.query_params
-        q_period = [q.get('start', None), q.get('end', None)]
-        # If any of the above query params are not specified, fall back to the settings values
-        # (Get the entire schedule)
-        if all([q is None for q in q_period]):
-            q_period = [settings.get(name='schedule/start').value, settings.get(name='schedule/end').value]
-        
-        # --- Compute all dates for requested period ---
-        # Turn dates into datetime objects
-        to_date = lambda datestr: datetime.datetime.strptime(datestr, '%m/%d/%Y').date()
-        dates = list(daterange(
-            *[ to_date(datestr) for datestr in q_period ]
-        ))
-        
-        # --- Get base events for each day---
-        base_events = dict()
-        weekday = lambda date: WEEK_DAYS[date.weekday()][0]
-        for date in dates:
-            print(weekday(date))
-            base_events[date] = Event.objects.filter(day=weekday(date))
-        
-        print(base_events)
-        
-        
-        
-        return computed_events
-    
