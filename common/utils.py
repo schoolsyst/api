a71a71992aad7d4535c631cf31a051911a72f349
Ewen Le Bihan <ewen.lebiha7@gmail.com>
2019-08-17 16:19:56 +0200

    remove mutable default arguments in auto_list_display helper

--- common/utils.py
+++ common/utils.py
@@ -1,10 +1,13 @@
-def auto_list_display(Model, add=[], exclude=[]):
+def auto_list_display(Model, add=None, exclude=None):
     from django.db.models.fields.related import ManyToManyField
-    
+
+    if add is None: add = list()
+    if exclude is None: exclude = list()
+        
     return [field.name for field in Model._meta.get_fields() # Get all fields
-            if field.__class__ is not ManyToManyField       # Remove Many-to-many fields
-            and field.name not in exclude] + add            # Add additional fields, remove excluded fields
-    
+        if field.__class__ is not ManyToManyField       # Remove Many-to-many fields
+        and field.name not in exclude] + add            # Add additional fields, remove excluded fields
+
 
 # @ https://www.w3resource.com/python-exercises/date-time-exercise/python-date-time-exercise-50.php
 def daterange(date1, date2):
